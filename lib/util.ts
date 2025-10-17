7693245faaa624300f8488a6959a3c76121b2975
maat <464896740@qq.com>
2025-10-17 12:44:48 +0800

    解决bug

--- lib/util.ts
+++ lib/util.ts
@@ -19,12 +19,21 @@ export const mapNewSearchToOld = (res: SearchItem, suffix = false) => {
   if (
     res &&
     res.price_promotion_info.final_promotion_path_list &&
-    res.price_promotion_info.final_promotion_path_list.final_promotion_path_map_data.find(item => item.promotion_title === '商品券' || item.promotion_title === '店铺券')
+    res.price_promotion_info.final_promotion_path_list.final_promotion_path_map_data.find(
+      (item) =>
+        item.promotion_title === '商品券' || item.promotion_title === '店铺券'
+    )
   ) {
-    const coupon = res.price_promotion_info.final_promotion_path_list.final_promotion_path_map_data[0];
+    const coupon =
+      res.price_promotion_info.final_promotion_path_list
+        .final_promotion_path_map_data[0];
     couponAmount = coupon.promotion_fee;
-    coupon_start_time = dayjs(Number(coupon.promotion_start_time)).format('YYYY-MM-DD HH:mm:ss');
-    coupon_end_time = dayjs(Number(coupon.promotion_end_time)).format('YYYY-MM-DD HH:mm:ss');
+    coupon_start_time = dayjs(Number(coupon.promotion_start_time)).format(
+      'YYYY-MM-DD HH:mm:ss'
+    );
+    coupon_end_time = dayjs(Number(coupon.promotion_end_time)).format(
+      'YYYY-MM-DD HH:mm:ss'
+    );
     coupon_id = coupon.promotion_id;
     promotion_desc = coupon.promotion_desc;
     coupon_start_fee = findStartFee(coupon.promotion_desc);
@@ -71,7 +80,8 @@ export const parseTbResult = (item: any, rate = 1) => {
   const _item = mapNewSearchToOld(item);
   const isMj = _item.coupon_start_fee * 1 > _item.zk_final_price * 1;
   const coupon = isMj ? 0 : _item.coupon_amount || 0;
-  const promotionPrice = Number((Number(_item.zk_final_price - coupon).toFixed(2))) * 1;
+  const promotionPrice =
+    Number(Number(_item.zk_final_price - coupon).toFixed(2)) * 1;
   const commissionRate = Number(_item.commission_rate);
   const couponList: any[] = [];
   if (_item.coupon_amount) {
@@ -96,7 +106,9 @@ export const parseTbResult = (item: any, rate = 1) => {
     coupon: couponList,
     commissionRate,
     user_type: _item.user_type,
-    commission: Number((((promotionPrice * commissionRate) / 100 / 100) * rate).toFixed(2)),
+    commission: Number(
+      (((promotionPrice * commissionRate) / 100 / 100) * rate).toFixed(2)
+    ),
     sales: _item.volume,
   };
   return res;
@@ -108,11 +120,23 @@ export const wrapIosPwd = (pwd: string, suffix = 'AC01') => {
 };
 
 export const handlePwd = (response: any) => {
-  const isValidInfo = response.data.material_url_list && response.data.material_url_list.material_url_list && response.data.material_url_list.material_url_list.length && Object.keys(response.data.material_url_list.material_url_list[0].link_info_dto).length;
+  const isValidInfo =
+    response.data.material_url_list &&
+    response.data.material_url_list.material_url_list &&
+    response.data.material_url_list.material_url_list.length &&
+    Object.keys(
+      response.data.material_url_list.material_url_list[0].link_info_dto
+    ).length;
   if (isValidInfo) {
     const parseInfo = response.data.material_url_list.material_url_list[0];
-    const bestPwd = parseInfo.coupon_info_dto && Object.keys(parseInfo.coupon_info_dto).length ? parseInfo.link_info_dto.coupon_short_tpwd : parseInfo.link_info_dto.cps_short_tpwd;
-    const bestUrl = parseInfo.coupon_info_dto && Object.keys(parseInfo.coupon_info_dto).length ? parseInfo.link_info_dto.coupon_long_url : parseInfo.link_info_dto.cps_long_url;
+    const bestPwd =
+      parseInfo.coupon_info_dto && Object.keys(parseInfo.coupon_info_dto).length
+        ? parseInfo.link_info_dto.coupon_short_tpwd
+        : parseInfo.link_info_dto.cps_short_tpwd;
+    const bestUrl =
+      parseInfo.coupon_info_dto && Object.keys(parseInfo.coupon_info_dto).length
+        ? parseInfo.link_info_dto.coupon_long_url
+        : parseInfo.link_info_dto.cps_long_url;
     const res: any = {
       coupon: parseInfo.coupon_info_dto,
       commission: parseInfo.promotion_info_dto,
@@ -123,14 +147,22 @@ export const handlePwd = (response: any) => {
         material_id: parseInfo.link_info_dto.material_id,
         material_type: parseInfo.link_info_dto.material_type,
         originUrl: parseInfo.link_info_dto.tpwd_origin_url,
-        model: parseInfo.link_info_dto.coupon_short_tpwd || parseInfo.link_info_dto.cps_short_tpwd,
-        password: parseInfo.link_info_dto.coupon_full_tpwd || parseInfo.link_info_dto.cps_full_tpwd,
-        short_url: parseInfo.link_info_dto.coupon_short_url || parseInfo.link_info_dto.cps_short_url,
+        model:
+          parseInfo.link_info_dto.coupon_short_tpwd ||
+          parseInfo.link_info_dto.cps_short_tpwd,
+        password:
+          parseInfo.link_info_dto.coupon_full_tpwd ||
+          parseInfo.link_info_dto.cps_full_tpwd,
+        short_url:
+          parseInfo.link_info_dto.coupon_short_url ||
+          parseInfo.link_info_dto.cps_short_url,
         click_url: parseInfo.link_info_dto.cps_long_url,
       },
     };
     if (parseInfo.link_info_dto.material_type === 1 && res.info.originUrl) {
-      const match = res.info.originUrl.match(/activityId=([\w\d]+?)(?:$|\b|\s|&)/);
+      const match = res.info.originUrl.match(
+        /activityId=([\w\d]+?)(?:$|\b|\s|&)/
+      );
       if (match) {
         res.info.activity_id = match[1];
       }
@@ -141,7 +173,12 @@ export const handlePwd = (response: any) => {
 };
 
 export const handlePage = (response: any) => {
-  const isValidInfo = response.data.event_url_list && response.data.event_url_list.event_url_list && response.data.event_url_list.event_url_list.length && Object.keys(response.data.event_url_list.event_url_list[0].link_info_dto).length;
+  const isValidInfo =
+    response.data.event_url_list &&
+    response.data.event_url_list.event_url_list &&
+    response.data.event_url_list.event_url_list.length &&
+    Object.keys(response.data.event_url_list.event_url_list[0].link_info_dto)
+      .length;
   if (isValidInfo) {
     const parseInfo = response.data.event_url_list.event_url_list[0];
     const res = {
@@ -162,15 +199,19 @@ export const handleItemId = (response: any) => {
     response.data.item_url_list &&
     response.data.item_url_list.item_url_list &&
     response.data.item_url_list.item_url_list.length &&
-    Object.keys(response.data.item_url_list.item_url_list[0].link_info_dto).length;
+    Object.keys(response.data.item_url_list.item_url_list[0].link_info_dto)
+      .length;
   if (isValidInfo) {
     const parseInfo = response.data.item_url_list.item_url_list[0];
     const link_info_dto = parseInfo.link_info_dto;
-    const rate = parseInfo.promotion_info_dto ? parseInfo.promotion_info_dto.commission_rate : '0';
+    const rate = parseInfo.promotion_info_dto
+      ? parseInfo.promotion_info_dto.commission_rate
+      : '0';
     const res = {
       coupon: parseInfo.coupon_info_dto,
       commission: parseInfo.promotion_info_dto,
       info: {
+        ...link_info_dto,
         coupon_click_url: link_info_dto.coupon_long_url,
         item_id: link_info_dto.item_id,
         item_url: link_info_dto.cps_long_url,
